2007c80db32478fc752263b0c6608dae8ccbb5b9
benixmaxedis <benwales@bigpond.com>
2023-11-08 12:48:42 +1000

    fixed eslint errors

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PixIcon from '@mui/icons-material/Pix';
 import { Box, Typography, useTheme } from '@mui/material';
 import FlexBetween from '@/components/FlexBetween';
-import PixIcon from '@mui/icons-material/Pix';
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState('dashboard');
-
   return (
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* LEFT SIDE */}
@@ -19,6 +16,7 @@ const Navbar = (props: Props) => {
           Finanseer
         </Typography>
       </FlexBetween>
+
       {/* RIGHT SIDE */}
       <FlexBetween gap="2rem">
         <Box sx={{ '&:hover': { color: palette.primary[100] } }}>
